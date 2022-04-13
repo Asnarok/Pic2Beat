7afb2286b0c7e8e5235328ec876ef55911e9f53b
Arthourrr <arthur.genieys@insa-lyon.fr>
2022-04-13 15:21:20 +0200

    Premiere version du générateur d'harmonie 3000 fonctionnelle (kk)

---
+++ src/pic2beat/Harmonie/Chord.java
@@ -0,0 +1,138 @@
+package pic2beat.Harmonie;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import jm.JMC;
+import jm.music.data.*;
+
+
+public class Chord {
+
+	public String Nom;
+	public String Type;
+	public Chord Previous;
+	public String[] Notes = new String[4]; 
+	public double[] Proba;	
+	//public int Duree;
+	private static ArrayList<String> NotesToInt = new ArrayList<>(List.of("A ", "A#", "B ", "C ", "C#", "D ", "D#", "E ", "F ", "F#", "G ", "G#")); 
+	private static ArrayList<String> TypeToInt = new ArrayList<>(List.of("Maj", "min", "Maj7", "min7", "7"));
+
+
+
+
+	public Chord (String nom) {
+		Nom = nom;
+		Notes[0]= Nom.substring(0, 2); //fondamentale
+		Type=Nom.substring(2); // Maj, min, Maj7, min7
+		switch (Type) {
+
+		case "Maj" :
+			Notes[1] = this.toNote(this.interval(Notes[0], 4));// NotesToInt.get(modulo(NotesToInt.indexOf(Notes[0])+4, 12));
+			Notes[2] = this.toNote(this.interval(Notes[1], 3));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[1])+3, 12));
+			break;
+		case "min" :
+			Notes[1] = this.toNote(this.interval(Notes[0], 3));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[0])+3, 12));
+			Notes[2] = this.toNote(this.interval(Notes[1], 4));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[1])+4, 12));
+			break;
+		case "Maj7" :
+			Notes[1] = this.toNote(this.interval(Notes[0], 4));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[0])+4, 12));
+			Notes[2] = this.toNote(this.interval(Notes[1], 3));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[1])+3, 12));
+			Notes[3] = this.toNote(this.interval(Notes[2], 4));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[2])+4, 12));
+			break;
+		case "min7" :
+			Notes[1] = this.toNote(this.interval(Notes[0], 3));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[0])+3, 12));
+			Notes[2] = this.toNote(this.interval(Notes[1], 4));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[1])+4, 12));
+			Notes[3] = this.toNote(this.interval(Notes[2], 3));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[2])+3, 12));
+			break;
+		case "7" :
+			Notes[1] = this.toNote(this.interval(Notes[0], 4));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[0])+4, 12));
+			Notes[2] = this.toNote(this.interval(Notes[1], 3));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[1])+3, 12));
+			Notes[3] = this.toNote(this.interval(Notes[2], 3));//NotesToInt.get(modulo(NotesToInt.indexOf(Notes[2])+4, 12));
+			break;
+		}
+
+		System.out.println(this.Nom);
+	}
+	public Chord (int note, int type) {
+		this("" + NotesToInt.get(note) + TypeToInt.get(type));
+	}
+	public Chord (int note0, int note1, int note2, int note3) {
+		Chord Maj = new Chord(toNote(note0)+"Maj");
+		if (toNum(Maj.Notes[0])== note0 && toNum(Maj.Notes[1])== note1 && toNum(Maj.Notes[2])== note2 && toNum(Maj.Notes[3])== note3) {
+			this.Nom=Maj.Nom;
+			this.Notes=Maj.Notes;
+			this.Type=Maj.Type;
+		} else {
+			Chord min = new Chord(toNote(note0)+"min");
+			if (toNum(min.Notes[0])== note0 && toNum(min.Notes[1])== note1 && toNum(min.Notes[2])== note2 && toNum(min.Notes[3])== note3) {
+				this.Nom=min.Nom;
+				this.Notes=min.Notes;
+				this.Type=min.Type;
+			} else {
+				Chord Maj7 = new Chord(toNote(note0)+"Maj7");
+				if (toNum(Maj7.Notes[0])== note0 && toNum(Maj7.Notes[1])== note1 && toNum(Maj7.Notes[2])== note2 && toNum(Maj7.Notes[3])== note3) {
+					this.Nom=Maj7.Nom;
+					this.Notes=Maj7.Notes;
+					this.Type=Maj7.Type;
+				} else {
+					Chord min7 = new Chord(toNote(note0)+"min7");
+					if (toNum(min7.Notes[0])== note0 && toNum(min7.Notes[1])== note1 && toNum(min7.Notes[2])== note2 && toNum(min7.Notes[3])== note3) {
+						this.Nom=min7.Nom;
+						this.Notes=min7.Notes;
+						this.Type=min7.Type;
+					} else {
+						Chord d7 = new Chord(toNote(note0)+"7");
+						if (toNum(d7.Notes[0])== note0 && toNum(d7.Notes[1])== note1 && toNum(d7.Notes[2])== note2 && toNum(d7.Notes[3])== note3) {
+							this.Nom=d7.Nom;
+							this.Notes=d7.Notes;
+							this.Type=d7.Type;
+						} 
+					} 
+				} 
+			} 
+		} 
+	}
+	public int modulo (int num, int modulo) {
+		while (num>=modulo) {
+			num-=modulo;
+		}
+		return num;
+	}
+	public String toString() {
+		String rep = new String();
+		for (int i =0; i< Notes.length; i++) {
+			rep += Notes[i]+ "  "; 
+		}
+		return rep;
+
+	}
+	public String toNote(int num) {
+		return NotesToInt.get(num);
+	}
+	public int toNum(String note) {
+		return NotesToInt.indexOf(note);
+	}
+	public int interval(int note, int interval) {
+		return modulo(note + interval, 12);
+	}
+	public int interval(String note, int interval) {
+		return modulo(toNum(note) + interval, 12);
+	}
+	public Chord interval (Chord c, int dist) {		// NOT IMPLEMENTED !!
+		return null;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Chord ))
+			return false;
+		return (this.Nom.equals(((Chord)obj).Nom) );
+		
+	}
+	@Override
+	public int hashCode() {		
+		return this.Nom.hashCode();
+	}
+	
+}
---
+++ src/pic2beat/Harmonie/MainTest.java
@@ -0,0 +1,146 @@
+package pic2beat.Harmonie;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class MainTest {
+	public static LinkedList<Chord> Progression = new LinkedList<>(); 
+	public static Chord Tonalite;
+	public static ArrayList<Chord> Degres = new ArrayList<>();
+	public Map<Chord, Map<Chord, Map<Chord, Integer>>> MatriceProba0 = new HashMap(); // abscisse = accord passé, ordonnée = présent, Zaxis = choix & pondé
+	public ArrayList<int[]> a;
+
+	public static void main (String [] args) {
+
+		final String MOOD = "HAPPY";
+		final int LENGTH = 10; //longueur du morceau en mesures
+
+		switch (MOOD) {
+		//initialisation de la tonalité
+		case "HAPPY" :
+			int fon = (int)(Math.random()*12); //entre 0 et 4
+			int typ = 0; // entre 0 et 4, 0 = Maj
+
+			Tonalite = new Chord(fon, typ);
+			Progression.add(Tonalite);
+			Map<Chord, Map<Chord, Integer>> Matrix = InitMatProba();
+
+
+			for (int mes =1; mes< LENGTH; mes++) {
+				System.out.println("LOOP starts");
+				System.out.println(Progression);
+				int sum = 0;
+				for(Entry<Chord, Integer> chord : Matrix.get(Progression.getLast()).entrySet() ) {
+					sum+= chord.getValue();
+				}
+				System.out.println("sum = "+sum);
+				int var = (int)(Math.random()*sum );
+				System.out.println("var = " +var);
+
+				ArrayList<Integer> temp = new ArrayList<>(Matrix.get(Progression.getLast()).values());
+				temp.sort(null);
+				System.out.println(temp);
+				int treshold =0;
+				int i=0;
+				
+				do {
+					treshold += temp.get(i);
+					i++;
+					System.out.println(i-1 +" "+ treshold);
+				}while ( i< temp.size()-1 && var> treshold);
+					
+						System.out.println("success");
+						for (Entry<Chord, Integer> set : Matrix.get(Progression.getLast()).entrySet() ) {
+							if (set.getValue()==temp.get(i-1)) {
+								Progression.add(set.getKey());
+								System.out.println("SUCCESS");
+								System.out.println(set.getKey());
+								break;
+							} else System.out.println("FAIL");
+						} 
+					
+				
+
+
+			}
+			System.out.println("Progression générée : ");
+			for(Chord c : Progression) {
+				System.out.print(c.Nom+", ");
+			}
+
+
+			/*
+			Degres.add(Tonalite);
+			//Degres.add(new Chord(fon+2, ));
+
+			break;
+		}
+		/*int a = (int)(Math.random()*12);
+		int b =(int)( Math.random()*4);
+		Progression.add(new Chord(a, b));
+		System.out.println(a +" "+b+"\n"+Progression.get(0).Nom +" "+ Progression.get(0).Type );
+			 */
+
+
+		}
+
+	}
+	public static Map<Chord, Map<Chord, Integer>> InitMatProba() {
+		Map<Chord, Map<Chord, Integer>> MatriceProba = new HashMap(); // abscisse = accord présent, ordonnée = choix possibles et pondérations
+
+
+		int numTon = Tonalite.toNum(Tonalite.Nom.substring(0, 2));
+		Map<Chord, Integer> deg1 = new HashMap<>();
+		deg1.put(Tonalite, 5); 				   //I	// 5% de chance de rester sur le même accord
+		deg1.put(new Chord(Tonalite.interval(numTon,7), 0), 24);//V	// Données récupérées sur https://www.hooktheory.com/trends#node=1&key=A
+		deg1.put(new Chord(Tonalite.interval(numTon,7), 4), 20);
+		deg1.put(new Chord(Tonalite.interval(numTon,5), 0), 28);//IV	// Et simplifiées en ne gardant que les choix les plus probables
+		deg1.put(new Chord(Tonalite.interval(numTon,9), 1), 15);//vi	// modulés pour atteindre 100%.
+		deg1.put(new Chord(Tonalite.interval(numTon,2), 1), 8); //ii
+		MatriceProba.put(Tonalite, deg1 );
+		System.out.println(Objects.equals(Tonalite, new Chord(Tonalite.toNum(Tonalite.Notes[0]), 0)));
+
+		Map<Chord, Integer> deg5 = new HashMap<>();
+		deg5.put(new Chord(Tonalite.interval(numTon,7), 0), 2);	//V	
+		deg5.put(new Chord(Tonalite.interval(numTon,5), 0), 25); //IV
+		deg5.put(new Chord(Tonalite.interval(numTon,9), 1), 34);	//vi
+		deg5.put(new Chord(Tonalite.interval(numTon,2), 1), 10);	//ii
+		deg5.put(new Chord(Tonalite.interval(numTon,0), 0), 14);	//I
+		MatriceProba.put(new Chord(Tonalite.interval(numTon,7), 0), deg5 );
+
+		Map<Chord, Integer> deg4 = new HashMap<>();
+		deg4.put(new Chord(Tonalite.interval(numTon,5), 0), 1); //IV
+		deg4.put(new Chord(Tonalite.interval(numTon,7), 0), 30);	//V	
+		deg4.put(new Chord(Tonalite.interval(numTon,9), 1), 12);	//vi
+		deg4.put(new Chord(Tonalite.interval(numTon,2), 1), 5);	//ii
+		deg4.put(new Chord(Tonalite.interval(numTon,0), 0), 32);	//I
+		MatriceProba.put(new Chord(Tonalite.interval(numTon,5), 0), deg4 );
+
+		Map<Chord, Integer> deg6 = new HashMap<>();
+		deg6.put(new Chord(Tonalite.interval(numTon,7), 0), 23);	//V	
+		deg6.put(new Chord(Tonalite.interval(numTon,5), 0), 32); //IV
+		deg6.put(new Chord(Tonalite.interval(numTon,9), 1), 0);	//vi
+		deg6.put(new Chord(Tonalite.interval(numTon,2), 1), 9);	//ii
+		deg6.put(new Chord(Tonalite.interval(numTon,0), 0), 11);	//I
+		deg6.put(new Chord(Tonalite.interval(numTon,4), 1), 8);	//iii
+		MatriceProba.put(new Chord(Tonalite.interval(numTon,9), 0), deg6 );
+
+		Map<Chord, Integer> deg2 = new HashMap<>();
+		deg2.put(new Chord(Tonalite.interval(numTon,7), 0), 10);	//V	
+		deg2.put(new Chord(Tonalite.interval(numTon,5), 0), 17); //IV
+		deg2.put(new Chord(Tonalite.interval(numTon,7), 1), 25);	//vi
+		deg2.put(new Chord(Tonalite.interval(numTon,7), 3), 1);	//ii7
+		deg2.put(new Chord(Tonalite.interval(numTon,7), 0), 19);	//I
+		deg2.put(new Chord(Tonalite.interval(numTon,7), 1), 15);	//iii
+		MatriceProba.put(new Chord(Tonalite.interval(numTon,2), 0), deg2 );
+		
+		Map<Chord, Integer> dom7 =new HashMap<>();
+		dom7.put(Tonalite, 100);
+		MatriceProba.put(new Chord(Tonalite.interval(numTon,7), 4), dom7);
+
+
+
+		System.out.println(MatriceProba.get(Tonalite));
+		return MatriceProba;
+	}
+}
